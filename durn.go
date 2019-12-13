742ef81f3240381f209a66f6eb1407b154b1343c
Jesper Larsson <jesper.a.larsson@outlook.com>
2019-12-13 20:22:24 +0100

    Integrate view into main package

--- durn.go
+++ durn.go
@@ -4,6 +4,7 @@ import (
 	"durn2/config"
 	"durn2/handler"
 	"durn2/middleware"
+	"durn2/view"
 	"fmt"
 	"log"
 	"net/http"
@@ -28,9 +29,21 @@ func setupLog() {
 	log.SetOutput(os.Stdout)
 }
 
-func createRouter() (router *mux.Router) {
+func createViewFactory() view.Factory {
+	return view.NewFactory(
+		config.Default.GetMust("DEFAULT_SITE_TITLE"),
+		config.Default.GetMust("WEB_TEMPLATE_PATH"),
+	)
+}
+
+func createRouter(viewFactory view.Factory) (router *mux.Router) {
+	stylePath := config.Default.GetMust("WEB_STYLE_PATH")
+
 	router = mux.NewRouter()
 	router.Use(middleware.Log.Access)
+
+	router.PathPrefix("/css/").
+		Handler(http.StripPrefix("/css/", http.FileServer(http.Dir(stylePath))))
 
 	helloWorld := handler.NewHelloWorld()
 	router.Handle("/", helloWorld)
@@ -53,7 +66,8 @@ func createServer(r *mux.Router) (srv *http.Server) {
 
 func main() {
 	setupLog()
-	r := createRouter()
+	vf := createViewFactory()
+	r := createRouter(vf)
 	srv := createServer(r)
 
 	log.Printf("Starting web server on %s", srv.Addr)
